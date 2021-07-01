c7e8f8baa9b1facbab61dcca8d90b6bbbb45363c
Masum Ahmed Choudhuri <42108289+Minor-lazer@users.noreply.github.com>
2021-07-01 21:38:33 +0530

    filter feature

--- my-app/src/ProductData.js
+++ my-app/src/ProductData.js
@@ -8,7 +8,12 @@ const product_card = [
         description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est, vero.",
         price:91,
         img:{photo1},
-        quantity:0
+        quantity:0,
+        inStock: true,
+		fastDelivery: false,
+		ratings: 4,
+		categories: ["Indoor Plant","Air Purifier"],
+		isNew: 0
     },
 
     {
@@ -17,7 +22,12 @@ const product_card = [
         descripton:"vbnm snbfkj jekfqwj  wjfbwfn",
         price:92,
         img:{photo1},
-        quantity:0
+        quantity:0,
+        inStock: true,
+		fastDelivery: false,
+		ratings: 1,
+		categories: ["Indoor Plant","Air Purifier"],
+		isNew: 0
     },
 
     {
@@ -26,7 +36,12 @@ const product_card = [
         description:"vbnm snbfkj jekfqwj  wjfbwfn",
         price:93,
         img: {photo1},
-        quantity:0
+        quantity:0,
+        inStock: true,
+		fastDelivery: true,
+		ratings: 5,
+		categories: ["Indoor Plant","Air Purifier"],
+		isNew: 1
     },
 
     {
@@ -35,7 +50,12 @@ const product_card = [
         description:"vbnm snbfkj jekfqwj  wjfbwfn",
         price:94,
         img:"https://massdrop-s3.imgix.net/product-images/drop-sennheiser-pc38x-gaming-headset/FP/CeH9BYVoTniYOsScfzZ0_PC.png?auto=format&fm=jpg&fit=fill&w=300&h=300&bg=f0f0f0&fill=solid&fill-color=f0f0f0&dpr=1&q=70",
-        quantity:0
+        quantity:0,
+        inStock: false,
+		fastDelivery: false,
+		ratings: 1,
+		categories: ["Indoor Plant","Air Purifier"],
+		isNew: 1
     },
 
     {
@@ -44,8 +64,55 @@ const product_card = [
         description:"vbnm snbfkj jekfqwj  wjfbwfn",
         price:95,
         img:{photo1},
-        quantity:0
-    }
+        quantity:0,
+        inStock: true,
+		fastDelivery: true,
+		ratings: 2,
+		categories: ["Indoor Plant","Air Purifier"],
+		isNew: 1
+    },
+
+    {
+        id:6,
+        name:"Tableau",
+        description:"vbnm snbfkj jekfqwj  wjfbwfn",
+        price:95,
+        img:{photo1},
+        quantity:0,
+        inStock: false,
+		fastDelivery: true,
+		ratings: 3,
+		categories: ["Indoor Plant","Air Purifier"],
+		isNew: 0
+    },
+
+    {
+        id:7,
+        name:"Tableau",
+        description:"vbnm snbfkj jekfqwj  wjfbwfn",
+        price:95,
+        img:{photo1},
+        quantity:0,
+        inStock: true,
+		fastDelivery: false,
+		ratings: 4,
+		categories: ["Indoor Plant","Air Purifier"],
+		isNew: 0
+    }, 
+
+    {
+        id:8,
+        name:"Tableau",
+        description:"vbnm snbfkj jekfqwj  wjfbwfn",
+        price:95,
+        img:{photo1},
+        quantity:0,
+        inStock: true,
+		fastDelivery: false,
+		ratings: 5,
+		categories: ["Indoor Plant","Air Purifier"],
+		isNew: 1
+    } 
 ]
 
 export default product_card;
